201fe18e018b413c0bb7e0a62f1ee77c2be5928f
Malte Hübner <maltehuebner@gmx.org>
2016-11-11 22:29:12 +0100

    Added some toArray methods.

--- src/Coord/Coord.php
+++ src/Coord/Coord.php
@@ -9,10 +9,42 @@ class Coord
 
     /** @var float $longitude */
     private $longitude;
-    
+
     public function __construct(float $latitude, float $longitude)
     {
         $this->latitude = $latitude;
         $this->longitude = $longitude;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            $this->latitude,
+            $this->longitude
+        ];
+    }
+
+    public function toInversedArray(): array
+    {
+        return [
+            $this->longitude,
+            $this->latitude
+        ];
+    }
+
+    public function toLatLngArray(): array
+    {
+        return [
+            'lat' => $this->latitude,
+            'lng' => $this->longitude
+        ];
+    }
+
+    public function toLatLonArray(): array
+    {
+        return [
+            'lat' => $this->latitude,
+            'lon' => $this->longitude
+        ];
     }
 }
