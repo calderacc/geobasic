cfc9f559ca4b012ed3b7ed5b737c4d11ed01c28d
Malte Hübner <maltehuebner@gmx.org>
2016-12-01 16:46:33 +0100

    Get PolylineConverter here.

---
+++ src/PolylineConverter/PolylineConverter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Caldera\GeoBasic\PolylineConverter;
+
+use Caldera\GeoBasic\Coord\Coord;
+use Caldera\GeoBasic\TrackInterface\TrackInterface;
+
+class PolylineConverter
+{
+    public static function getCoordList(TrackInterface $track): array
+    {
+        $pointList = \Polyline::decode($track->getPolyline());
+        $coordList = [];
+
+        while (count($pointList) > 0) {
+            $latitude = array_shift($pointList);
+            $longitude = array_shift($pointList);
+
+            $coord = new Coord($latitude, $longitude);
+
+            array_push($coordList, $coord);
+        }
+
+        return $coordList;
+    }
+}
