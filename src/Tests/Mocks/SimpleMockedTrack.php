b670c3ca0cf9c9dc9dd0f1d8b4219220294a18a5
Malte Hübner <maltehuebner@gmx.org>
2016-12-01 16:39:52 +0100

    Add SimpleMockedTrack.

---
+++ src/Tests/Mocks/SimpleMockedTrack.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Caldera\GeoBasic\Tests\Mocks;
+
+use Caldera\GeoBasic\TrackInterface\TrackInterface;
+
+class SimpleMockedTrack implements TrackInterface
+{
+    public function setPolyline(string $polyline): TrackInterface
+    {
+        // TODO: Implement setPolyline() method.
+    }
+
+    /**
+     * This mocks a 17 waypoint route around the Binnenalster in Hamburg.
+     *
+     * @return string
+     */
+    public function getPolyline(): string
+    {
+        return 'wrzeIuy~{@WdEmBrDmCvAwCuB_D}BaEyBy@yBHoG~@_HrAuItAeG`ByCt@a@`AlAxQv^{@nDwBjB';
+    }
+}
