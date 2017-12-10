3e2d8920699e9212ef930ee5da56c7c628a85ac4
Malte Hübner <maltehuebner@gmx.org>
2017-12-10 12:45:27 +0100

    Strip most of the stuff out of Coord.

--- src/Coord/Coord.php
+++ src/Coord/Coord.php
@@ -10,71 +10,19 @@ class Coord implements CoordInterface
     /** @var float $longitude */
     protected $longitude;
 
-    public function __construct(float $latitude, float $longitude)
+    public function __construct(float $latitude = null, float $longitude = null)
     {
         $this->latitude = $latitude;
         $this->longitude = $longitude;
     }
 
-    public function getLatitude(): float
+    public function getLatitude(): ?float
     {
         return $this->latitude;
     }
 
-    public function getLongitude(): float
+    public function getLongitude(): ?float
     {
         return $this->longitude;
-    }
-
-    public function toArray(): array
-    {
-        return [
-            $this->latitude,
-            $this->longitude
-        ];
-    }
-
-    public function toInversedArray(): array
-    {
-        return [
-            $this->longitude,
-            $this->latitude
-        ];
-    }
-
-    public function toLatLngArray(): array
-    {
-        return [
-            'lat' => $this->latitude,
-            'lng' => $this->longitude
-        ];
-    }
-
-    public function toLatLonArray(): array
-    {
-        return [
-            'lat' => $this->latitude,
-            'lon' => $this->longitude
-        ];
-    }
-
-    public function northOf(Coord $coord): bool
-    {
-        return $this->latitude > $coord->getLatitude();
-    }
-
-    public function southOf(Coord $coord): bool
-    {
-        return $this->latitude < $coord->getLatitude();
-    }
-
-    public function westOf(Coord $coord): bool
-    {
-        return $this->longitude < $coord->getLongitude();
-    }
-
-    public function eastOf(Coord $coord): bool
-    {
-        return $this->longitude > $coord->getLongitude();
     }
 }
--- src/Coord/CoordInterface.php
+++ src/Coord/CoordInterface.php
@@ -4,16 +4,6 @@ namespace Caldera\GeoBasic\Coord;
 
 interface CoordInterface
 {
-    public function getLatitude(): float;
-    public function getLongitude(): float;
-
-    public function toArray(): array;
-    public function toInversedArray(): array;
-    public function toLatLngArray(): array;
-    public function toLatLonArray(): array;
-
-    public function northOf(Coord $coord): bool;
-    public function southOf(Coord $coord): bool;
-    public function westOf(Coord $coord): bool;
-    public function eastOf(Coord $coord): bool;
+    public function getLatitude(): ?float;
+    public function getLongitude(): ?float;
 }
